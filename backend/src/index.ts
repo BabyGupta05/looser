1f43d3a6301698fb08b68bc62db735d156cadd99
BabyGupta05 <codecode355@gmail.com>
2024-12-31 02:17:20 +0530

    db setup in the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,14 @@
-import express from "express";
-import connectDB from "./db/db";
+import express from 'express';
+
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.get("/api/hello", (req, res) => {
-  res.send("Hello from the backend!");
+app.get('/api/hello', (req, res) => {
+  res.send('Hello from the backend!');
 });
 
-app.listen(port, async () => {
-  await connectDB();
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
